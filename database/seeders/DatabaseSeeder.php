571bfc5d3aa7f96e909a22194f3f96de875b10c1
Iomar Gorges <iomargorges@hotmail.com>
2021-07-23 10:05:31 -0300

    feat(seeders/DatabaseSeeder): run InitialSeeder and ParamLocaleSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(InitialSeeder::class);
+        $this->call(ParamLocaleSeeder::class);
     }
 }
